e55012944d8640ad7ddd95e59bbdc75618aaef88
Luis Gonzalez <luisleonardo2@gmail.com>
2018-10-06 09:00:50 -0400

    COMMIT INICIAL

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>OneStorage</title>
+
+    <!-- Librerias CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">OneStorage</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template mt-5">
+        <h1>OneStorage</h1>
+        <p class="lead">Alquiler de Depósitos Seguros y Privados</p>
+<div class="alert alert-warning" role="alert">
+  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> En Construcción!
+</div>
+      </div>
+
+    </main><!-- /.container -->
+
+
+
+	<!-- Footer -->
+	<footer class="footer">
+	<div class="container">
+	    <span class="">© <?= date('Y') ?> OneStorage</span>
+	</div>
+	</footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+</html>
