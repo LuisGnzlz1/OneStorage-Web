4252cf5d7f74dae82c8178b1b67552f1a4f8b64b
Luis Gonzalez <luisleonardo2@gmail.com>
2018-10-22 22:59:04 -0400

    NUEVOS CAMBIOS, AGREGANDO GALERIA, ANIMACIONES, DETALLEES CON EL RESPONSIVE

--- index.php
+++ index.php
@@ -9,11 +9,30 @@
     <title>OneStorage</title>
 
     <!-- Librerias CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/sweetalert2.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
+
+    <?php if($_SERVER['SERVER_NAME'] == 'localhost'){  ?>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/sweetalert2.min.css">
+        <link rel="stylesheet" href="css/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+        <link rel="stylesheet" href="css/aos.css">
+
+    <?php }else{ ?>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.5/sweetalert2.min.css">
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+
+    <?php } ?>
+
+
+        <link rel="stylesheet" href="css/main.css">
 
 </head>
 
@@ -29,14 +48,14 @@
                 <div class="row">
 
                     <div class="col-sm-12 text-center">
-                        <h1 class="title-slogan">Alquiler de Depósitos y Bauleras</h1>
+                        <h1 data-aos="zoom-in" data-aos-duration="2000" class="title-slogan">Alquiler de Depósitos y Bauleras</h1>
                     </div>
 
                 </div>
 
                 <div class="row">
                     <div class="col-sm-12 text-center">
-                        <img src="img/logo.png" class="logo-one img-fluid">
+                        <img data-aos="zoom-in" data-aos-duration="1000" src="img/logo.png" class="logo-one img-fluid">
                     </div>
                 </div>
 
@@ -44,33 +63,6 @@
 
         </div>
     </div>
-
-    <div class="jumbotron jumbotron-fluid s0-responsive parallax">
-        <div class="container">
-
-            <div class="header">
-
-                <div class="row">
-
-                    <div class="col-sm-12 text-center">
-                        <h1 class="title-slogan">Alquiler de Depósitos y Bauleras</h1>
-                    </div>
-
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-12 text-center">
-                        <img src="img/logo.png" class="logo-one img-fluid">
-                    </div>
-                </div>
-
-            </div>
-
-
-        </div>
-
-    </div>
-
 
     <div class="row s1">
 
@@ -163,7 +155,7 @@
             <div class="col-sm-6 text-left d-flex align-items-center">
 
                 <div class="animatedParent">
-                    <img class="img-fluid animated bounceInRight" src="img/box8.png">
+                    <img class="img-fluid" data-aos="zoom-in" data-aos-duration="1000" src="img/box8.png">
                 </div>
 
             </div>
@@ -193,7 +185,7 @@
             <div class="col-sm-6 text-left d-flex align-items-center">
 
                 <div class="animatedParent">
-                    <img class="img-fluid animated bounceInRight" src="img/box15.png">
+                    <img class="img-fluid" data-aos="zoom-in" data-aos-duration="1000" src="img/box15.png">
                 </div>
 
             </div>
@@ -223,7 +215,7 @@
             <div class="col-sm-6 text-left d-flex align-items-center">
 
                 <div class="animatedParent">
-                    <img class="img-fluid animated bounceInRight" src="img/box30.png">
+                    <img class="img-fluid" data-aos="zoom-in" data-aos-duration="1000" src="img/box30.png">
                 </div>
 
             </div>
@@ -260,9 +252,7 @@
 
             <div class="col-sm-12 text-center">
 
-                <div class="animatedParent">
-                    <img class="img-fluid" src="img/box8.png">
-                </div>
+                <img class="img-fluid" data-aos="zoom-in" data-aos-duration="1000" src="img/box8.png">
 
             </div>
 
@@ -285,9 +275,7 @@
 
             <div class="col-sm-12 text-center">
 
-                <div class="animatedParent">
-                    <img class="img-fluid" src="img/box15.png">
-                </div>
+                <img class="img-fluid" data-aos="zomm-in" data-aos-duration="1000" src="img/box15.png">
 
             </div>
 
@@ -310,9 +298,7 @@
 
             <div class="col-sm-12 text-center">
 
-                <div class="animatedParent">
-                    <img class="img-fluid" src="img/box30.png">
-                </div>
+                <img class="img-fluid" data-aos="zoom-in" data-aos-duration="1000" src="img/box30.png">
 
             </div>
 
@@ -320,7 +306,7 @@
 
     </div>
 
-    <div class="row green-bg s3 text-center">
+    <div class="row green-bg s3 mx-auto text-center" style="padding: 15px">
 
         <div class="col-sm-12">
 
@@ -328,12 +314,15 @@
                 <img class="logo-one-2 img-fluid" src="img/onestorage.png">
             </div>
 
+        </div>
+
+        <div class="col-sm-12">
             <h1 class="display-5 green-bg-title">Guardamos tus cosas</h1>
         </div>
 
         <div class="col-xs-12 col-sm-6 col-md-4">
 
-            <img class="img-fluid" src="img/ico01.png">
+            <img class="img-fluid" data-aos="zoom-in-down" data-aos-duration="1000" src="img/ico01.png">
             <h4 class="text-center">Contenedores</h4>
             <p class="text-icon">Marítimos de acero, herméticos, individuales para cada cliente</p>
 
@@ -341,62 +330,68 @@
 
         <div class="col-xs-12 col-sm-6 col-md-4">
 
-            <img class="img-fluid" src="img/ico02.png">
+            <img class="img-fluid" data-aos="zoom-in-down" data-aos-duration="1000" src="img/ico02.png">
             <h4 class="text-center">Seguridad</h4>
             <p class="text-icon">Doble cierre de seguridad, llaves de acceso en su poder únicamente</p>
 
         </div>
 
-        <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="col-xs-12 col-sm-12 col-md-4">
 
-            <img class="img-fluid" src="img/ico03.png">
+            <img class="img-fluid" data-aos="zoom-in-down" data-aos-duration="1000" src="img/ico03.png">
             <h4 class="text-center">Video Remoto</h4>
             <p class="text-icon">Por parte del cliente a través de la APP de su Smartphone (iOS/Android)</p>
 
         </div>
 
+    </div>
+
+
+
+    <div class="mt-5 mb-4 mx-auto">
+
+        <div class="col-sm-12 text-center">
+            <h1 class="text-center">Galería</h1>
+        </div>
+
+        <div class="gallery">
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V1.jpg">
+            </div>
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V2.jpg">
+            </div>
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V3.jpg">
+            </div>
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V4.jpg">
+            </div>
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V5.jpg">
+            </div>
+
+            <div>
+                <img class="img-fluid" src="img/calesita/V6.jpg">
+            </div>
+
+        </div>
+
 
     </div>
 
 
-    <div class="row green-bg s3-responsive mx-auto">
+    <hr class="text-center mb-2 mt-5" style="width: 60%">
 
-        <div class="col-sm-12">
-
-            <div class="text-center mt-5 mb-4">
-                <img class="logo-one-2 img-fluid" src="img/onestorage.png">
-            </div>
-
-        </div>
-
-        <div class="col-sm-12">
-            <h1 class="display-5 green-bg-title">Guardamos tus cosas</h1>
-        </div>
-
-        <div class="col-sm-12 text-center">
-            <div class="animatedParent">
-                <img class="img-fluid" src="img/icono01.png">
-            </div>
-        </div>
-
-        <div class="col-sm-12 text-center">
-            <div class="animatedParent">
-                <img class="img-fluid" src="img/icono02.png">
-            </div>
-        </div>
-
-        <div class="col-sm-12 text-center">
-            <div class="animatedParent">
-                <img class="img-fluid" src="img/icono033.png">
-            </div>
-        </div>
-
-
-    </div>
 
     <div class="row s4">
 
-        <div class="col-sm-6 mt-4">
+        <div class="col-xs-12 col-sm-12 col-md-6 mt-4">
 
             <div class="card mx-auto card-contacto">
                 <div class="card-body text-center" style="background-color: #fff">
@@ -415,9 +410,9 @@
 
         </div>
 
-        <div class="col-sm-6 mt-5">
+        <div class="col-xs-12 col-sm-12 col-md-6 mt-5">
 
-            <div class="form-contact">
+            <div class="form-contact mx-auto">
 
                 <form id="contact-form" role="form">
 
@@ -459,74 +454,7 @@
 
     </div>
 
-    <div class="row s4-responsive">
-
-        <div class="col-sm-12 mt-4">
-
-            <div class="card mx-auto card-contacto">
-                <div class="card-body text-center" style="background-color: #fff">
-                    <h1 class="display-5 info-title mb-2"> Contacto</h1>
-                    <p class="info-text">
-                        Lunes a Sabado de 7hs a 21hs. <br><br>
-
-                        <i class="fa fa-envelope" aria-hidden="true"></i> info@onestorage.com.ar<br><br>
-                        <i class="fa fa-phone" aria-hidden="true"></i> (11) 15-5317-2062 (Diego)<br>
-                        <i class="fa fa-phone" aria-hidden="true"></i> (11) 15-5807-6277 (Viviana)<br>
-                        <i class="fa fa-phone" aria-hidden="true"></i> (11) 15-6748-7404 (Javier)<br><br>
-                    </p>
-                    <div class="figura-box mx-auto"></div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-sm-12 mt-4">
-
-            <div class="form-contact mx-auto">
-
-                <form id="contact-form-r" role="form">
-
-                    <h1>Envianos tu consulta</h1>
-
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <input id="c-nombre-r" type="text" name="c-nombre-r" class="form-control" placeholder="Ingrese su nombre">
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <input id="c-mail-r" type="text" name="c-mail-r" class="form-control" placeholder="Ingrese su correo">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <textarea id="c-mensaje-r" name="c-mensaje-r" class="form-control" placeholder="Ingrese un mensaje" rows="4"></textarea>
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <button type="button" id="btn-send-mensaje-r" class="btn btn-green btn-send btn-block">
-                                Enviar mensaje
-                            </button>
-                        </div>
-                    </div>
-
-                </form>
-
-            </div>
-
-
-        </div>
-
-    </div>
+    <hr class="text-center mb-2 mt-5" style="width: 60%">
 
     <div class="row mt-5 s5">
 
@@ -547,13 +475,31 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
 
+
     <script src="http://maps.google.com/maps/api/js?key=AIzaSyBTaATiiD1Bwt8NShPxW0W1F3N63NMII-M"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.25/gmaps.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-    <script src="js/sweetalert2.min.js"></script>
+
+    <?php if($_SERVER['SERVER_NAME'] == 'localhost'){  ?>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/sweetalert2.min.js"></script>
+        <script src="js/aos.js"></script>
+        <script src="js/slick.min.js"></script>
+
+    <?php }else{ ?>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.5/sweetalert2.min.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <?php } ?>
 
     <script src="js/main.js"></script>
+
+
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127724155-1"></script>
